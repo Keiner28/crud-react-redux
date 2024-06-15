c0d23f814ecbdabf06931ace4a8408102e27039c
Keiner28 <keinergodinez@gmail.com>
2024-06-14 18:12:05 -0600

    feat: :sparkles: Create table with tremor library and action buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import { ListOfUsers } from './components/ListOfUsers'
 
 function App() {
   return (
-    <>
-      <h1 className='text-red-700'>Hola desde react</h1>
-    </>
+    <div className='flex flex-col gap-4'>
+      <h1 className='text-red-700'>Proyecto de React con redux</h1>
+      <ListOfUsers />
+    </div>
   )
 }
 
